cd561459e1cd3d2e896446af4bc1144b67872d9e
valdisl0ve <75386722+valdisl0ve@users.noreply.github.com>
2021-04-19 20:54:30 +0300

    Update Client.cs

--- Client.cs
+++ Client.cs
@@ -1,103 +1,60 @@
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.Intrinsics.X86;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Server
+namespace Client
 {
-    
     internal static class Program
-    { 
-        static int global;
-        static Socket opponent;
+    {
         private static void Main()
         {
-            string[] field = new string[9] {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
             var ip = IPAddress.Parse("127.0.0.1");
             const int PORT = 8005;
             var ipServer = new IPEndPoint(ip, PORT);
-            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            listen.Bind(ipServer);
-            listen.Listen(10);
+            var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Connect(ipServer);
+            var message = " ";
+            string[] field = new string[9] {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
             
-           
-            
-            
-            
-            //Task.Run(() => MainTask(listen));
-            
-            string flag = "X";
-            string winer = " ";
-            ShowField(field);
-            var client = listen.Accept();
-            opponent = client;
-            
-           // Task.Run(() => NewClient(client));
-            
-            do
+            while (true)
             {
-                if (flag == "X")
-                {
-                    
-                    Console.WriteLine();
-                    Console.WriteLine("YOUR TURN [X] PLAYER, ENTER THE NUM OF CELL");
-                    int cell_num = Convert.ToInt32(Console.ReadLine());
-                    var data = Encoding.Unicode.GetBytes(Convert.ToString(cell_num));
-                   
-                    opponent.Send(data);
-                    
-                    
-                    UpdateField(cell_num, flag, field);
-                    ShowField(field);
-                    
-                    if (WinCondition(flag, field) == false)
-                    {flag = "O";}
-                    else
-                    {winer = flag; }
-                    
-                }
-                
-                
+                ShowField(field);
                
-              
                 
+                int bytes = 0;
                 
-                if (flag == "O")
+                string msg;
+                var dataSend = new byte[256];
+                
+                do
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("YOUR TURN [O] PLAYER, ENTER THE NUM OF CELL");
-                    string message;
-                    var data1 = new byte[256];
-                    do
-                    {
-                        var bytes = client.Receive(data1);
-                        message = Encoding.Unicode.GetString(data1, 0, bytes);
-                    } while (client.Available > 0);
-                    var temp = "PLAYER [O] TURN IN CELL № - " + message ;
-                    Console.WriteLine(temp);
-                    global = Convert.ToInt32(message);
-                    
-                    
-                    Console.ReadKey();
-                    int cell_num = global;
-                    UpdateField(cell_num, flag, field);
-                    ShowField(field);
-                    
-                    if (WinCondition(flag, field) == false)
-                    {flag = "X";}
-                    else 
-                    {winer = flag;}
-                }
-            } while (winer == " ");
-            
-            Console.WriteLine();
-            Console.WriteLine("ENDGAME");
-            Console.WriteLine(flag + " - WINNER");
-            
+                    bytes = server.Receive(dataSend);
+                    msg =  Encoding.Unicode.GetString(dataSend, 0, bytes);
+                } while (server.Available > 0);
+                Console.WriteLine();
+                Console.WriteLine("X PLAYER TURN IN CELL № - " +msg);
+                int cell_num = Convert.ToInt32(msg);
+                //string player = "X";
+                
+                UpdateField( cell_num, "X",  field);
+                ShowField(field);
+                
+                
+                
+                
+                message = Console.ReadLine();
+                var data = Encoding.Unicode.GetBytes(message);
+                server.Send(data);
+                UpdateField( Convert.ToInt32(message), "O",  field);
+                
+                
+            }
         }
+        
+        
+        
+        
         
         static void ShowField(string[] field)
         {
@@ -124,37 +81,9 @@ namespace Server
                 }
             }
         }
-      
         
         
         
-        static bool WinCondition(string symbol, string[] field)
-        {
-
-            if (
-                (field[0] == symbol && field[1] == symbol && field[2] == symbol) ||
-                (field[3] == symbol && field[4] == symbol && field[5] == symbol) ||
-                (field[6] == symbol && field[7] == symbol && field[8] == symbol) ||
-
-                (field[0] == symbol && field[3] == symbol && field[6] == symbol) ||
-                (field[1] == symbol && field[4] == symbol && field[7] == symbol) ||
-                (field[2] == symbol && field[5] == symbol && field[8] == symbol) ||
-
-                (field[0] == symbol && field[4] == symbol && field[8] == symbol) ||
-                (field[6] == symbol && field[4] == symbol && field[2] == symbol)
-
-            )
-            {
-                return true;
-            }
-                
-            
-            
-            else {return false;}
-            
-            
-            
-        } 
         
     }
 }
